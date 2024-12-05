e8144687ab5f15dd6390948f3edf276ae1a7bccc
Joni Ananta <154442692+Udyana30@users.noreply.github.com>
2024-12-05 13:00:55 +0800

    Update MainActivity.kt

--- app/app/src/main/java/com/capstone/surevenir/MainActivity.kt
+++ app/app/src/main/java/com/capstone/surevenir/MainActivity.kt
@@ -4,6 +4,7 @@ package com.capstone.surevenir
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,52 +14,70 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.capstone.surevenir.data.network.response.CreateUserRequest
 import com.capstone.surevenir.helper.UserPreferences
+import com.capstone.surevenir.ui.camera.CameraScreen
 import com.capstone.surevenir.ui.camera.ImageCaptureVM
 import com.capstone.surevenir.ui.camera.PreviewScreen
-import com.capstone.surevenir.ui.splash.AllCategoryScreen
-import com.capstone.surevenir.ui.splash.AllHistory
-import com.capstone.surevenir.ui.splash.AllProductScreen
-import com.capstone.surevenir.ui.splash.AllShopScreen
-import com.capstone.surevenir.ui.splash.BottomNavigationBar
-import com.capstone.surevenir.ui.splash.FavoritesScreen
-import com.capstone.surevenir.ui.splash.FloatingButtonWithIntent
-import com.capstone.surevenir.ui.splash.ForgotPassword
-import com.capstone.surevenir.ui.splash.Home
-import com.capstone.surevenir.ui.splash.OnBoardingScreen
-import com.capstone.surevenir.ui.splash.ProfileScreen
-import com.capstone.surevenir.ui.splash.ResultScreen
-import com.capstone.surevenir.ui.splash.ScanScreen
-import com.capstone.surevenir.ui.splash.ShopScreen
-import com.capstone.surevenir.ui.splash.SignInScreen
-import com.capstone.surevenir.ui.splash.SignUpScreen
-import com.capstone.surevenir.ui.splash.SingleProductScreen
-import com.capstone.surevenir.ui.splash.SplashScreen
-import com.capstone.surevenir.ui.splash.StickyTopBar
-import com.capstone.surevenir.ui.splash.profile.AccountCenterScreen
-import com.capstone.surevenir.ui.splash.profile.EditProfileScreen
-import com.capstone.surevenir.ui.splash.profile.SettingsScreen
+import com.capstone.surevenir.ui.screen.allscreen.AllCategoryScreen
+import com.capstone.surevenir.ui.screen.allscreen.AllHistory
+import com.capstone.surevenir.ui.screen.allscreen.AllProductScreen
+import com.capstone.surevenir.ui.screen.allscreen.AllShopScreen
+import com.capstone.surevenir.ui.screen.navmenu.BottomNavigationBar
+import com.capstone.surevenir.ui.screen.navmenu.TransactionScreen
+import com.capstone.surevenir.ui.screen.navmenu.FloatingButtonWithIntent
+import com.capstone.surevenir.ui.screen.forgotpassword.ForgotPassword
+import com.capstone.surevenir.ui.screen.navmenu.Home
+import com.capstone.surevenir.ui.screen.onboarding.OnBoardingScreen
+import com.capstone.surevenir.ui.screen.navmenu.ProfileScreen
+import com.capstone.surevenir.ui.screen.ResultScreen
+import com.capstone.surevenir.ui.screen.allscreen.AllMarket
+import com.capstone.surevenir.ui.screen.mylocation.MyLocationScreen
+import com.capstone.surevenir.ui.screen.navmenu.ScanScreen
+import com.capstone.surevenir.ui.screen.navmenu.ShopScreen
+import com.capstone.surevenir.ui.screen.navmenu.SignInScreen
+import com.capstone.surevenir.ui.screen.signup.SignUpScreen
+import com.capstone.surevenir.ui.screen.singlescreen.SingleCategoryScreen
+import com.capstone.surevenir.ui.screen.singlescreen.SingleProductScreen
+import com.capstone.surevenir.ui.screen.singlescreen.SingleShopScreen
+import com.capstone.surevenir.ui.screen.splash.SplashScreen
+import com.capstone.surevenir.ui.screen.navmenu.StickyTopBar
+import com.capstone.surevenir.ui.screen.profile.AccountCenterScreen
+import com.capstone.surevenir.ui.screen.profile.EditProfileScreen
+import com.capstone.surevenir.ui.screen.profile.SettingsScreen
 import com.capstone.surevenir.ui.theme.MyAppTheme
+import com.capstone.surevenir.ui.viewmodel.UserViewModel
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.util.UUID
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     companion object {
         private const val GOOGLE_SIGN_IN_CODE = 1001
@@ -80,70 +99,116 @@ class MainActivity : ComponentActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == GOOGLE_SIGN_IN_CODE) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
-                val account = task.getResult(ApiException::class.java)
-                println("Google Sign-In Success: ${account.displayName}")
-                firebaseAuthWithGoogle(account.idToken!!, navController, this)
+                val userViewModel: UserViewModel = ViewModelProvider(this)[UserViewModel::class.java]
+                handleGoogleSignInResult(data, this, navController, userViewModel)
             } catch (e: ApiException) {
                 println("Google Sign-In failed: ${e.statusCode}")
                 println("Error message: ${e.message}")
                 e.printStackTrace()
             }
         }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
     }
 
 }
 
 
 
-private fun firebaseAuthWithGoogle(idToken: String, navController: NavHostController, context: Context) {
+private fun handleGoogleSignInResult(
+    data: Intent?,
+    context: Context,
+    navController: NavController,
+    userViewModel: UserViewModel
+) {
+    val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+    try {
+        val account = task.getResult(ApiException::class.java)
+        if (account != null) {
+            val email = account.email
+            val displayName = account.displayName
+            val firebaseUser = FirebaseAuth.getInstance().currentUser
+            val firebaseUid = firebaseUser?.uid ?: UUID.randomUUID().toString()
 
+            val dummyToken = "b1025941-5a1d-4d86-bef0-d05e9118befb"
 
-    val credential = GoogleAuthProvider.getCredential(idToken, null)
-    FirebaseAuth.getInstance().signInWithCredential(credential)
-        .addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                val user = task.result?.user
-                val token = user?.uid
+            Log.d("DEBUG", "Using Dummy Token: $dummyToken")
+
+            userViewModel.getUsers(dummyToken) { users ->
+                val existingUser = users?.find { it.email == email }
+                Log.d("DEBUG", "Fetched Users: $users")
+                Log.d("DEBUG", "Existing User: $existingUser")
 
                 val userPreferences = UserPreferences(context)
                 (context as ComponentActivity).lifecycleScope.launch {
-                    userPreferences.saveLoginState(isLoggedIn = true, token = token ?: "", email = user?.email ?: "")
-                }
+                    if (existingUser != null) {
+                        userPreferences.saveLoginState(
+                            isLoggedIn = true,
+                            token = existingUser.id,
+                            email = email ?: ""
+                        )
+                        Log.d("DEBUG", "Existing user found. ID: ${existingUser.id}")
+                    } else {
+                        val request = CreateUserRequest(
+                            id = firebaseUid,
+                            fullName = displayName ?: "Unknown Name",
+                            username = displayName ?: "Unknown Username",
+                            email = email ?: "unknown_email@gmail.com",
+                            password = "google_login",
+                            phone = "",
+                            role = "USER",
+                            provider = "GOOGLE",
+                            longitude = "",
+                            latitude = "",
+                            address = ""
+                        )
+                        userViewModel.createUser(request)
 
-                navController.navigate("home") {
-                    popUpTo("splash") { inclusive = true }
+                        userPreferences.saveLoginState(
+                            isLoggedIn = true,
+                            token = firebaseUid,
+                            email = email ?: ""
+                        )
+                        Log.d("DEBUG", "New user created with ID: $firebaseUid")
+                    }
+
+                    navController.navigate("home") {
+                        popUpTo("signIn") { inclusive = true }
+                    }
                 }
-            } else {
-                println("Sign-In Failed: ${task.exception?.message}")
-                task.exception?.printStackTrace()
             }
         }
+    } catch (e: ApiException) {
+        e.printStackTrace()
+        Log.e("DEBUG", "Google Sign-In failed: ${e.message}")
+    }
 }
+
+
 
 
 @Composable
 fun MainScreen(navController: NavHostController) {
-    // Mendapatkan rute saat ini
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
     val imageCaptureViewModel: ImageCaptureVM = viewModel()
+    val context = LocalContext.current
+    val cameraExecutor = remember { ContextCompat.getMainExecutor(context) }
 
     Scaffold(
         topBar = {
-            if (currentRoute in listOf("home", "shop", "scan", "favorites", "profile")) {
-                StickyTopBar()
+            if (currentRoute in listOf("home", "shop", "scan", "transaction", "profile", "myLocation")) {
+                StickyTopBar(navController)
             }
         },
         bottomBar = {
-            if (currentRoute in listOf("home", "shop", "scan", "favorites", "profile")) {
+            if (currentRoute in listOf("home", "shop", "scan", "transaction", "profile")) {
                 BottomNavigationBar(navController)
             }
         },
         floatingActionButton = {
-            // Only show FAB on main screens
-            if (currentRoute in listOf("home", "shop", "scan", "favorites", "profile")) {
+            if (currentRoute in listOf("home", "shop", "scan", "transaction", "profile")) {
                 FloatingButtonWithIntent(
                     navController = navController,
                     imageCaptureViewModel = imageCaptureViewModel
@@ -164,6 +229,9 @@ fun MainScreen(navController: NavHostController) {
                     navigateToHome = { navController.navigate("home") { popUpTo("splash") { inclusive = true } } },
                     navigateToSignIn = { navController.navigate("signIn") { popUpTo("splash") { inclusive = true } } }
                 )
+            }
+            composable("myLocation") {
+                MyLocationScreen(navController)
             }
             composable("onboarding") {
                 OnBoardingScreen(navController = navController)
@@ -186,8 +254,8 @@ fun MainScreen(navController: NavHostController) {
             composable("scan") {
                 ScanScreen(navController)
             }
-            composable("favorites") {
-                FavoritesScreen(navController)
+            composable("transaction") {
+                TransactionScreen(navController)
             }
             composable("profile") {
                 ProfileScreen(navController)
@@ -204,9 +272,10 @@ fun MainScreen(navController: NavHostController) {
             composable("allHistory") {
                 AllHistory(navController)
             }
-            composable("singleProduct") {
-                SingleProductScreen(navController)
+            composable("allMarket") {
+                AllMarket(navController)
             }
+
             composable("preview") {
                 PreviewScreen(
                     navController = navController,
@@ -230,9 +299,36 @@ fun MainScreen(navController: NavHostController) {
             composable("settings") {
                 SettingsScreen(navController)
             }
+            composable("singleCategory") {
+                SingleCategoryScreen(navController)
+            }
+            composable(
+                "product/{productId}",
+                arguments = listOf(navArgument("productId") { type = NavType.IntType })
+            ) { backStackEntry ->
+                val productId = backStackEntry.arguments?.getInt("productId") ?: 0
+                SingleProductScreen(productId, navController)
+            }
+            composable(
+                "merchant/{merchantId}",
+                arguments = listOf(navArgument("merchantId") { type = NavType.IntType })
+            ) { backStackEntry ->
+                val merchantId = backStackEntry.arguments?.getInt("merchantId") ?: 0
+                SingleShopScreen(merchantId, navController)
+            }
+
+            composable("camera") {
+                CameraScreen(
+                    navController = navController,
+                    imageCaptureViewModel = imageCaptureViewModel,
+                    executor = cameraExecutor
+                )
+            }
         }
     }
 }
+
+
 
 
 @Preview(showBackground = true)
