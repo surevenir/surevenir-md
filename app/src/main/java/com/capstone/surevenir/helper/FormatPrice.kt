f976426d65396eeab494ead68fe069246c5f661b
ikadeknandasanjaya <nandacomp1075@gmail.com>
2024-12-05 13:25:04 +0800

    onboarding screen to be appeared 2

--- app/src/main/java/com/capstone/surevenir/helper/FormatPrice.kt
+++
@@ -1,8 +0,0 @@
-package com.capstone.surevenir.helper
-
-import java.text.DecimalFormat
-
-fun formatPrice(price: Int?): String {
-    val decimalFormat = DecimalFormat("#,###")
-    return decimalFormat.format(price ?: 0)
-}
--- app/src/main/java/com/capstone/surevenir/model/Image.kt
+++
@@ -1,9 +0,0 @@
-package com.capstone.surevenir.model
-
-
-data class Image(
-    val id: Int,
-    val url: String,
-    val itemId: Int,
-    val type: String
-)
